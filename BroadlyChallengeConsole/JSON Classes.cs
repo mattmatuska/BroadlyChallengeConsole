ab0806144a6532fe6f8f5004bd1493087ad27b05
toooskies <toooskies@gmail.com>
2016-01-05 20:47:42 -0800

    Read JSON files and write to console.

---
+++ BroadlyChallengeConsole/JSON Classes.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BroadlyChallengeConsole
+{
+    // classes acquired via PasteSpecial->Paste JSON as classes
+    // then, renamed.
+
+    public class ChallengePage
+    {
+        public string note { get; set; }
+        public string url { get; set; }
+    }
+
+
+    public class ClassList
+    {
+        public string note { get; set; }
+
+        //public string[] classes { get; set; }
+        // modifying to List<string>
+        public List<string> classes { get; set; }
+
+    }
+
+
+    public class Class
+    {
+        public string note { get; set; }
+        public string room { get; set; }
+        public Student[] students { get; set; }
+        public string next { get; set; }
+    }
+
+    public class Student
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public string id { get; set; }
+    }
+
+}
---
+++ BroadlyChallengeConsole/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Helpers;
+
+namespace BroadlyChallengeConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string class_list_json = GetJSON("http://challenge.broadly.com/classes");
+            ClassList list = Json.Decode(class_list_json, typeof(ClassList));
+            Console.WriteLine(class_list_json);//list.note);
+
+            if (list != null)
+            {
+                foreach (string class_page in list.classes)
+                {
+                    string class_json = GetJSON(class_page);
+                    Class local_class = Json.Decode(class_json, typeof(Class));
+                    Console.WriteLine(class_json);//local_class.note);
+
+                    while (!string.IsNullOrEmpty(local_class.next))
+                    {
+                        class_json = GetJSON(local_class.next);
+                        local_class = Json.Decode(class_json, typeof(Class));
+                        Console.WriteLine(class_json);//local_class.note);
+                    }
+                }
+            }
+
+            Console.ReadKey();
+        }
+
+        private static string GetJSON(string WebAddress)
+        {
+            // code borrowed from:
+            // https://msdn.microsoft.com/en-us/library/456dfw4f(v=vs.110).aspx
+
+            // Create a request for the URL. 
+            WebRequest request = WebRequest.Create(WebAddress);
+            // If required by the server, set the credentials.
+            request.Credentials = CredentialCache.DefaultCredentials;
+            // Get the response.
+            WebResponse response = request.GetResponse();
+            // Display the status.
+            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+            // Get the stream containing content returned by the server.
+            Stream dataStream = response.GetResponseStream();
+            // Open the stream using a StreamReader for easy access.
+            StreamReader reader = new StreamReader(dataStream);
+            // Read the content.
+            string responseFromServer = reader.ReadToEnd();
+            // Display the content.
+            //Console.WriteLine(responseFromServer);
+            // Clean up the streams and the response.
+            reader.Close();
+            response.Close();
+
+            return responseFromServer;
+        }
+    }
+}
