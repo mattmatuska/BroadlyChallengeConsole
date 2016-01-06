01b9aaa03c9708833ea6864658195cc9cdc5b368
toooskies <toooskies@gmail.com>
2016-01-05 21:27:32 -0800

    Do the math.

--- BroadlyChallengeConsole/Program.cs
+++ BroadlyChallengeConsole/Program.cs
@@ -13,25 +13,38 @@ namespace BroadlyChallengeConsole
     {
         static void Main(string[] args)
         {
+            int total_students_over_25 = 0;
             string class_list_json = GetJSON("http://challenge.broadly.com/classes");
             ClassList list = Json.Decode(class_list_json, typeof(ClassList));
-            Console.WriteLine(class_list_json);//list.note);
+            // Console.WriteLine(list.note);
 
             if (list != null)
             {
                 foreach (string class_page in list.classes)
                 {
+                    int class_students_over_25;
+
                     string class_json = GetJSON(class_page);
                     Class local_class = Json.Decode(class_json, typeof(Class));
-                    Console.WriteLine(class_json);//local_class.note);
+                    class_students_over_25 = local_class.students.Count(x => x.age >= 25);
+
+                    // Console.WriteLine(local_class.note);
 
                     while (!string.IsNullOrEmpty(local_class.next))
                     {
                         class_json = GetJSON(local_class.next);
                         local_class = Json.Decode(class_json, typeof(Class));
-                        Console.WriteLine(class_json);//local_class.note);
+                        // Console.WriteLine(local_class.note);
+
+                        class_students_over_25 += local_class.students.Count(x => x.age >= 25);
                     }
+
+                    total_students_over_25 += class_students_over_25;
+                    Console.WriteLine("room: {0} SO25: {1}", local_class.room, class_students_over_25);
                 }
+
+                Console.WriteLine("total classes: {0} total SO25: {1} avg SO25: {2}",
+                    list.classes.Count, total_students_over_25, total_students_over_25 / list.classes.Count);
             }
 
             Console.ReadKey();
