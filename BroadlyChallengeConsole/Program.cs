0a6db70335f6d5533602fd8505cb261ec5345304
toooskies <toooskies@gmail.com>
2016-01-05 23:05:36 -0800

    Refactored to isolate JSON acquisition and decoding for optimization.  These would be the two most time-consuming behaviors.

--- BroadlyChallengeConsole/Program.cs
+++ BroadlyChallengeConsole/Program.cs
@@ -15,10 +15,13 @@ namespace BroadlyChallengeConsole
         {
             try
             {
-                string class_list_json = GetJSON("http://challenge.broadly.com/classes");
+                Func<string, string> get_json = web_address => GetJsonWebRequest(web_address);
+                Func<string, Class> decode_json = json_text => Json.Decode(json_text, typeof(Class));
+
+                string class_list_json = get_json.Invoke(("http://challenge.broadly.com/classes"));
                 ClassList list = Json.Decode(class_list_json, typeof(ClassList));
 
-                Console.WriteLine("{0}", list.Classes.Average(s => GetNumStudents(s)));
+                Console.WriteLine("{0}", list.Classes.Average(s => GetNumStudents(s, get_json, decode_json)));
             }
             catch (Exception e) { Console.WriteLine(e.ToString()); }
             
@@ -31,17 +34,17 @@ namespace BroadlyChallengeConsole
         /// </summary>
         /// <param name="ClassPage">HTTP address of the page.</param>
         /// <returns></returns>
-        private static int GetNumStudents(string ClassPage)
+        private static int GetNumStudents(string ClassPage, Func<string, string> GetJson, Func<string, Class> DecodeJson)
         {
             if (!string.IsNullOrEmpty(ClassPage))
             {
                 try
                 {
-                    string class_json = GetJSON(ClassPage);
+                    string class_json = GetJson(ClassPage);
                     Class local_class = Json.Decode(class_json, typeof(Class));
 
                     return local_class.Students.Count(x => x.Age >= 25) +
-                        GetNumStudents(local_class.Next);
+                        GetNumStudents(local_class.Next, GetJson, DecodeJson);
                 }
                 catch (Exception e) { Console.WriteLine(e.ToString()); }
             }
@@ -54,7 +57,7 @@ namespace BroadlyChallengeConsole
         /// </summary>
         /// <param name="WebAddress"></param>
         /// <returns></returns>
-        private static string GetJSON(string WebAddress)
+        private static string GetJsonWebRequest(string WebAddress)
         {
             // code borrowed from:
             // https://msdn.microsoft.com/en-us/library/456dfw4f(v=vs.110).aspx
@@ -66,7 +69,7 @@ namespace BroadlyChallengeConsole
             // Get the response.
             WebResponse response = request.GetResponse();
             // Display the status.
-            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+            //Console.WriteLine(((HttpWebResponse)response).StatusDescription);
             // Get the stream containing content returned by the server.
             Stream dataStream = response.GetResponseStream();
             // Open the stream using a StreamReader for easy access.
